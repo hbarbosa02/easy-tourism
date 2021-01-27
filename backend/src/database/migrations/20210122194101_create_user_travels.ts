8cec07cee0dadcc10c6f0a41bbf387cee9e74ca6
Renan Bertoldo <renan.rbertoldo@gmail.com>
2021-01-26 21:23:00 -0300

    feat: added user travels tabçe

--- backend/src/database/migrations/20210122194101_create_user_travels.ts
+++ backend/src/database/migrations/20210122194101_create_user_travels.ts
@@ -1,10 +1,18 @@
-import * as Knex from "knex";
-
+import Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('user_travels', table => {
+    table.increments('id').primary();
+    table.integer('user_id').notNullable();
+    table.foreign('user_id').references('users.id');
+    table.integer('travel_id').notNullable();
+    table.foreign('travel_id').references('travels.id');
+    table.string('transaction_id').nullable();
+    table.float('amount').notNullable();
+    table.timestamps(false, true);
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('user_travels');
 }
-
